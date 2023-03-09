31a0f4706e7bc54bf41eca37874374414f559d00
Lucas Vacari <85653812+BrookSK@users.noreply.github.com>
2023-03-09 18:28:37 -0300

    Update

--- js/click-scroll.js
+++ js/click-scroll.js
@@ -1,7 +1,7 @@
 //jquery-click-scroll
 //by syamsul'isul' Arifin
 
-var sectionArray = [1, 2, 3, 4, 5];
+var sectionArray = [1, 2, 3, 4, 5, 6];
 
 $.each(sectionArray, function(index, value){
           
